879aa76e8c8951f7af99a410006ac804a7f45ce6
FOund404 <2247124474@qq.com>
2022-07-18 10:54:02 +0800

    7.18代码更新

--- app.js
+++ app.js
@@ -5,33 +5,23 @@ const fs = require('fs');
 // 导入cheerio工具解析模块
 const cheerio = require('cheerio');
 
-https.get('https://movie.douban.com/top250', function(res) {
-
-    console.log(res);
-
-    // 获取html文件
+let req = https.request('https://movie.douban.com/chart', res => {
     let html = '';
-    res.on('data', function(chunk) {
+    res.on('data', chunk => {
         html += chunk;
     });
-
-
-    res.on('end', function() {
-        // console.log(html);
-        const $ = cheerio.load(html);
-        let allFilms = [];
-        $('li .item').each(function() {
-            const title = $('.title', this).text();
-            const star = $('.rating_num', this).text();
-            const pic = $('.pic img', this).attr('src');
-            allFilms.push({ title, star, pic });
+    res.on('end', () => {
+        // 结束数据监听后将所有内容拼接存放进html
+        let $ = cheerio.load(html);
+        let text = [];
+        // 将筛选的内容存放到text数组
+        $('.article .indent .item').each(function(i, ele) {
+            const imgs = $('.nbg img', this).attr('src');
+            text.push({
+                pic: imgs
+            });
         });
-        fs.writeFile('./file.json', JSON.stringify(allFilms), function(err) {
-            if (!err) {
-                console.log('文件写入成功！');
-            }
-        });
-        downloadImg(allFilms);
+        downloadImg(text);
     });
 });
 // 编写下载方法
@@ -40,17 +30,24 @@ function downloadImg(allFilms) {
     for (let i = 0; i < allFilms.length; i++) {
         // 获取每个对象的url地址给picUrl
         const picUrl = allFilms[i].pic;
-
         https.get(picUrl, function(res) {
             // 读取picUrl写入到本地
-            // 二进制存放
+            // 设置二进制存放
             res.setEncoding('binary');
             let str = '';
             res.on('data', function(chunk) {
                 str += chunk;
             });
             res.on('end', function() {
-                // 写入模块、
+                // 写入模块
+                // 创建images文件夹
+                fs.mkdir('./images', function(err) {
+                    if (!err) {
+                        console.log('imgages文件写入成功！');
+                    }
+                });
+                // 往images文件中写入图片文件
+                // str, 'binary' 将str转换为二进制存放
                 fs.writeFile(`./images/${i}.png`, str, 'binary', function(err) {
                     if (!err) {
                         console.log(`第${i}张图片下载成功`);
@@ -59,4 +56,6 @@ function downloadImg(allFilms) {
             })
         })
     }
-}
+};
+// 结束请求
+req.end();
